74848063a9c251ff75ae7bb3a88308ffde148dbc
jpapric <jovana.papric2@gmail.com>
2026-05-25 10:07:48 +0200

    add client placeholder

---
+++ ClientVodenko/ClientVodenko/Helpers/GenericCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ClientVodenko.Helpers
+{
+    public class GenericCommand : ICommand
+    {
+        private readonly Func<Task> _execute;
+        private bool _isExecuting;
+
+        public GenericCommand(Func<Task> execute)
+        {
+            _execute = execute;
+            _isExecuting = false;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return !_isExecuting;
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (_isExecuting)
+                return;
+
+            _isExecuting = true;
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+            try
+            {
+                await _execute();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Error: {ex.Message}",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+            finally
+            {
+                _isExecuting = false;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
